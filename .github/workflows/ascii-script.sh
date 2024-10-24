d3085954d5f6f76c83bc8d548adb85a070fa0838
hariprasath <hariguru798@gmail.com>
2024-10-25 00:06:21 +0530

    added cowsay

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,6 @@
 #/bin/sh
-sudo apt-get install cowsay -y
+sudo apt-get update
+sudo apt-get install cowsay
 cowsay -f dragon "Run for cover ,I am a dragon ...RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
